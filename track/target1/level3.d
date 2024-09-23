0750ec4cd3639de5389c1ef8a3728d848ccea34e
1e4ves <916210345@qq.com>
2024-09-23 15:26:16 +0800

    2024-9-23-15-24

---
+++ track/target1/level3.d
@@ -0,0 +1,11 @@
+
+level3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	49 c7 c4 fa 18 40 00 	mov    $0x4018fa,%r12
+   7:	41 54                	push   %r12
+   9:	48 c7 c7 78 dc 61 55 	mov    $0x5561dc78,%rdi
+  10:	c3                   	ret    
